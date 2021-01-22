1e17a555a9e5f0c3d99177d507d1723dff4ed845
progriro <progriro@gmail.com>
2021-01-22 21:06:17 +0900

    [created] sample components

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,15 @@
-mport * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import {SubComponent} from './sub-component';
+import React from "react";
+import { createElement } from "react";
+import { render } from "react-dom";
+import { SubComponent } from "./sub-component";
 
-class App extends React.Component {
-  render() {
-    return (
-        <div>
-          <h1>Hello React!</h1>
-          <SubComponent name="My Counter for TypeScript"/>
-        </div>
-    );
-  }
-}
+export const App = () => {
+  return (
+    <div>
+      <h1>Hello React!</h1>
+      <SubComponent name="My Counter for TypeScript" />
+    </div>
+  );
+};
 
-ReactDOM.render(<App/>, document.querySelector('#app'));
+render(createElement(App), document.getElementById("app"));
