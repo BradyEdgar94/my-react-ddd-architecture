653d1d827d97e6741cc026269788b1b67c6c5e3e
progriro <progriro@gmail.com>
2021-01-22 21:20:18 +0900

    [modified] main.tsx for Pirce.ts

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { createElement } from "react";
 import { render } from "react-dom";
-import { SubComponent } from "./sub-component";
+import { Price } from "./Domain/ValueObject/Price";
 
 export const App = () => {
+  const [price, setPrice] = useState<Price>(new Price(0));
+  const [input, setInput] = useState("");
+
+  const handleClick = () => {
+    const inputNum = Number(input);
+    if (!inputNum) return;
+    const inputPrice = new Price(Number(inputNum));
+    setPrice(inputPrice);
+  };
+
   return (
     <div>
       <h1>Hello React!</h1>
-      <SubComponent name="My Counter for TypeScript" />
+      <p>{price}</p>
+      <input
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Input Price."
+      />
+      <button onClick={handleClick}>Click me</button>
     </div>
   );
 };
