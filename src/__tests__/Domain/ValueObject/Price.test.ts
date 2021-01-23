52b8d04348980d944a02726f642dfb8162b37f65
progriro <progriro@gmail.com>
2021-01-24 03:26:33 +0900

    [created] Price.test.ts

---
+++ src/__tests__/Domain/ValueObject/Price.test.ts
@@ -0,0 +1,14 @@
+import { Price } from "../../../Domain/ValueObject";
+
+describe("Domain/ValueObject/Price", () => {
+  test("0より小さい値が渡されたらエラーを吐く", () => {
+    expect(() => new Price(-100)).toThrowError(new Error(`不正な金額 : -100`));
+    expect(new Price(0)).toBeInstanceOf(Price);
+    expect(new Price(100)).toBeInstanceOf(Price);
+  });
+
+  test("Price.valueで金額が取得できる", () => {
+    const price = new Price(100);
+    expect(price.value).toBe(100);
+  });
+});
