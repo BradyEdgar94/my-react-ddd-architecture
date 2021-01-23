0cf70bccb2d69ad0f18e3e801b7bfca623ccf053
progriro <progriro@gmail.com>
2021-01-24 03:08:34 +0900

    [created] jest.config.ts

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from "@jest/types";
+
+// Sync object
+const config: Config.InitialOptions = {
+  verbose: true,
+  testMatch: ["**/__tests__/**/*.test.+(ts|tsx|js|jsx)"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  coveragePathIgnorePatterns: ["/node_modules/"],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+};
+export default config;
