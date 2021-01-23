cf92dbd775cbc4ad8bb1bb9a98a61759679a472a
progriro <progriro@gmail.com>
2021-01-24 02:25:33 +0900

    [add] webpack-dev-server's config into webpack.confing.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,18 @@
+const path = require("path");
+
 module.exports = {
   // モード値を production に設定すると最適化された状態で、
   // development に設定するとソースマップ有効でJSファイルが出力される
   mode: "development",
 
   // メインとなるJavaScriptファイル（エントリーポイント）
-  entry: "./src/main.tsx",
+  entry: "./src/main.ts",
   // ファイルの出力設定
   output: {
     //  出力ファイルのディレクトリ名
-    path: `${__dirname}/dist`,
+    path: path.join(__dirname, "dist"),
     // 出力ファイル名
-    filename: "main.js"
+    filename: "main.js",
   },
   module: {
     rules: [
@@ -18,14 +20,24 @@ module.exports = {
         // 拡張子 .ts もしくは .tsx の場合
         test: /\.tsx?$/,
         // TypeScript をコンパイルする
-        use: "ts-loader"
-      }
-    ]
+        use: "ts-loader",
+      },
+    ],
   },
   // import 文で .ts や .tsx ファイルを解決するため
   resolve: {
-    extensions: [".ts", ".tsx", ".js", ".json"]
+    extensions: [".ts", ".tsx", ".js", ".json"],
   },
   // ES5(IE11等)向けの指定（webpack 5以上で必要）
   target: ["web", "es5"],
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 8080,
+    inline: true,
+    hot: true,
+    open: true,
+    watchContentBase: true,
+  },
 };
