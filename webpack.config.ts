acc42d8c47fa8f5052c8f4023b131c4d70d9be1a
progriro <progriro@gmail.com>
2021-01-24 22:51:08 +0900

    [fixed] format in webpack.config.ts

--- webpack.config.ts
+++ webpack.config.ts
@@ -7,7 +7,6 @@ const config: webpack.Configuration = {
   // モード値を production に設定すると最適化された状態で、
   // development に設定するとソースマップ有効でJSファイルが出力される
   mode: "development",
-
   // メインとなるJavaScriptファイル（エントリーポイント）
   entry: "./src/main.ts",
   // ファイルの出力設定
