9c1fe06ced0b6f1ce655575ddac9bd29d8e326cc
progriro <progriro@gmail.com>
2021-02-13 00:11:00 +0900

    [modified] lazy import page for wouter

--- src/app.tsx
+++ src/app.tsx
@@ -1,27 +1,10 @@
-import React, { useState } from "react";
-import { Price } from "./Domain/ValueObject/Price";
+import React from "react";
+import { Router } from "./router";
 
 export const App = () => {
-  const [price, setPrice] = useState<Price>(new Price(0));
-  const [input, setInput] = useState("");
-
-  const handleClick = () => {
-    const inputNum = Number(input);
-    if (!inputNum) return;
-    const inputPrice = new Price(Number(inputNum));
-    setPrice(inputPrice);
-  };
-
   return (
-    <div>
-      <h1>Hello React!</h1>
-      <p>{price.value}</p>
-      <input
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-        placeholder="Input Price."
-      />
-      <button onClick={handleClick}>Click me</button>
-    </div>
+    <>
+      <Router />
+    </>
   );
 };
