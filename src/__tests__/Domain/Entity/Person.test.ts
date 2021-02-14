15ec664c861701208ad6a03efaa53fad97ee65af
progriro <progriro@gmail.com>
2021-02-14 16:29:14 +0900

    [modified] PersonFactoryからPersonを生成

--- src/__tests__/Domain/Entity/Person.test.ts
+++ src/__tests__/Domain/Entity/Person.test.ts
@@ -1,12 +1,9 @@
-import { Age, FullName, Height, Weight } from "../../../Domain/ValueObject";
 import { Person } from "../../../Domain/Entity";
+import { PersonFactory } from "../../../Domain/Factory";
+import { personObj } from "../../MockObjects";
 
 describe("Domain/Entity/Person", () => {
-  const name = new FullName("firstName", "lastName");
-  const age = new Age(25);
-  const height = new Height(174);
-  const weight = new Weight(68);
-  const person = new Person(1, name, age, height, weight);
+  const person = PersonFactory.createFromObj(personObj);
 
   test("Personが取得できる", () => {
     expect(person).toBeInstanceOf(Person);
