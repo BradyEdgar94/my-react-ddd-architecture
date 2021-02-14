71595eeb6888ff621f46304deda3787535fa3316
progriro <progriro@gmail.com>
2021-02-14 16:05:08 +0900

    [add] personId to test/Entity/Person.ts

--- src/__tests__/Domain/Entity/Person.test.ts
+++ src/__tests__/Domain/Entity/Person.test.ts
@@ -1,17 +1,18 @@
-import { Age, Height, Weight } from "../../../Domain/ValueObject";
+import { Age, FullName, Height, Weight } from "../../../Domain/ValueObject";
 import { Person } from "../../../Domain/Entity";
 
 describe("Domain/Entity/Person", () => {
+  const name = new FullName("firstName", "lastName");
   const age = new Age(25);
   const height = new Height(174);
   const weight = new Weight(68);
-  const person = new Person("john", age, height, weight);
+  const person = new Person(1, name, age, height, weight);
 
   test("Personが取得できる", () => {
     expect(person).toBeInstanceOf(Person);
   });
 
-  test("Person.nameで名前が取得できる", () => {
-    expect(person.name).toBe("john");
+  test("Person.personIdでpersonIdが取得できる", () => {
+    expect(person.personId).toBe(1);
   });
 });
