2c1933282052d5baf24e6cc3a8f6a096bd6b084e
progriro <progriro@gmail.com>
2021-02-14 16:28:41 +0900

    [created] MockObjects/Person.ts

---
+++ src/__tests__/MockObjects/Person.ts
@@ -0,0 +1,10 @@
+import { PersonObj } from "../../Domain/Factory";
+
+export const personObj: PersonObj = {
+  person_id: 1,
+  first_name: "firstName",
+  last_name: "lastName",
+  age: 22,
+  height: 175,
+  weight: 68,
+};
