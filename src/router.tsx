ed231df432a67b5218ab7a4b95c5ab8706285a5e
progriro <progriro@gmail.com>
2021-02-19 21:52:52 +0900

    [add] serivce path を追記

--- src/router.tsx
+++ src/router.tsx
@@ -3,6 +3,7 @@ import { Switch, Route } from "wouter";
 
 const Home = lazy(() => import("./Components/page/Home"));
 const About = lazy(() => import("./Components/page/About"));
+const Service = lazy(() => import("./Components/page/Service"));
 
 export const Router = () => {
   return (
@@ -10,6 +11,7 @@ export const Router = () => {
       <Switch>
         <Route path="/" component={Home} />
         <Route path="/about" component={About} />
+        <Route path="/service" component={Service} />
       </Switch>
     </Suspense>
   );
